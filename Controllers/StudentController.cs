0a728bf1e05f47a810e3aae638b7ca83289df000
khoatngguyen <nguyenvankkhoat2000@gmail.com>
2022-09-14 16:44:14 +0700

    NVK_519

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,17 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
-
+using PTPMQL.Models;
 namespace PTPMQL.Controllers
 {
     public class StudentController : Controller
     {
         // 
         // GET: /HelloWorld/
-
-        public string Index()
+       public IActionResult Index()
+    {
+        //Khoiwjtaor list student
+        List<Student> stdList=new List<Student>()
         {
-            return "This is my default action...";
-        }
+            new Student {StudentID=1, StudentName= "Nguyen Van A", Age= 18},
+            new Student {StudentID=2, StudentName="Nguyen Van B", Age=18},
+            new Student {StudentID=3, StudentName="Nguyen Van C", Age=18},
+            new Student {StudentID=4, StudentName="Nguyen Van D", Age=18},
+            new Student {StudentID=5, StudentName="Nguyen Van E", Age=18},
+            new Student {StudentID=6, StudentName="Nguyen Van F", Age=18},
+        };
+        ViewData["Student"]= stdList;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(StudentController std)
+    {
+        // string message = std.StudentID +"-";
+        // message += std.StudentName + "-";
+        // mesage += std.Age;
+        // ViewBag.TT = message;
+        return View();
+    }
+        
 
         // 
         // GET: /HelloWorld/Welcome/ 
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+namespace PTPMQL.Models;
+public class Student
+{
+    public int StudentID {get; set;}
+    public string StudentName {get; set;}
+    public int Age {get; set;}
+    
+}
